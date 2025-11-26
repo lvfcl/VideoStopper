4cdc092f4811d89a4d36bb51330029138c2dd648
lvfcl <83322580+lvfcl@users.noreply.github.com>
2025-11-26 01:00:03 +0100

    update

--- train.py
+++ train.py
@@ -5,7 +5,8 @@ DATASET_PATH = './YOLO/data.yaml'
 
 results = model.train(
     data=DATASET_PATH, 
-    epochs=100, 
+    epochs=300, 
+    patience=0,
     imgsz=640, 
     name='stop_detector'
 )
